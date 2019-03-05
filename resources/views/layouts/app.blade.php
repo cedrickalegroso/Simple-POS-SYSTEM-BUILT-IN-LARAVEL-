a5f4df1bcaba30c29e8f35e6088e865c1fea3b07
Dancedrick <personal.dancedrick@gmail.com>
2019-03-05 22:27:14 +0800

    added offline frameworks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="{{ URL::to('src/css/bootstrap.min.css') }} ">
+
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
